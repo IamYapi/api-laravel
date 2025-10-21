d434aa13e065d0c251994099046dd65415e15a83
yapie <yafiradifan11@gmail.com>
2025-10-21 21:22:35 +0700

    REST

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Todo;
 use Illuminate\Http\Request;
+
+
 
 class TodoController extends Controller
 {
@@ -12,7 +15,7 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+     return Todo::orderByDesc('id')->get();
     }
 
     /**
@@ -20,30 +23,35 @@ class TodoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate(['title' => 'required|string|max:100','completed' => 'boolean']);
+        $todo = Todo::create($data);
+        return response()->json($todo, 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Todo $todo)
     {
-        //
+        return $todo;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Todo $todo)
     {
-        //
+        $data = $request->validate(['title' => 'sometimes|string|max:100','completed' => 'sometimes|boolean']);
+        $todo->update($data);
+        return $todo;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Todo $todo)
     {
-        //
+        $todo->delete();
+        return response()->noContent(); 
     }
 }
